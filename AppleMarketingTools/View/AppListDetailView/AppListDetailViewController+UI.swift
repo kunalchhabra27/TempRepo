ef9d06f19591d0907b686f526fe65f560ad0789c
Kunal Chhabra <kunal.chhabra27@gmail.com>
2024-05-09 12:54:21 +0530

    Remove unwanted code, Add spacing between artistNameLbl and releaseDataLbl, Add text alignment to labels

--- AppleMarketingTools/View/AppListDetailView/AppListDetailViewController+UI.swift
+++ AppleMarketingTools/View/AppListDetailView/AppListDetailViewController+UI.swift
@@ -23,7 +23,8 @@ extension AppListDetailViewController {
                                      artistNameLbl.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0.0),
                                      artistNameLbl.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0.0),
                                      artistNameLbl.heightAnchor.constraint(equalToConstant: 20.0),
-                                     releaseDataLbl.topAnchor.constraint(equalTo: artistNameLbl.bottomAnchor, constant: 0.0),
+                                     
+                                     releaseDataLbl.topAnchor.constraint(equalTo: artistNameLbl.bottomAnchor, constant: 10.0),
                                      releaseDataLbl.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 0.0),
                                      releaseDataLbl.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: 0.0),
                                      releaseDataLbl.heightAnchor.constraint(equalToConstant: 18.0)
